9e23e2c877d364f0c47f0ab9277afabe45a549fd
Preston Hansen <prestonhansen01@gmail.com>
2016-04-21 19:27:51 -0500

    http request works now
    use something like:
    import requests
    
    payload = {‘location’ : location …. etc }
    r = requests.get(ADDRESS:PORT/query,params=payload)
    
    to test

--- flaskroulette/roulette.py
+++ flaskroulette/roulette.py
@@ -22,16 +22,15 @@ def index(name=None):
     # r = requests.get('https://127.0.0.1:5000/query',params=payload)
     return render_template('layout.html',name=name)
 
-@app.route('/query', methods='GET')
+@app.route('/query', methods=['GET'])
 def query():
     error = None
     if request.method =='GET':
-        if valid_request(request.form['location'],
-                        request.form['description'],
-                        request.form['time']):
-            return fulfill_query_request(request.form['location'],
-                            request.form['description'],
-                            request.form['time'])
+        location, description, time = request.args.get('location',''),\
+                                      request.args.get('description',''),\
+                                      request.args.get('time','')
+        if valid_request(location,description,time):
+            return fulfill_query_request(location,description,time)
         else:
             #handle error on bad request
             #(maybe just do this on the client side)
@@ -62,15 +61,31 @@ def fulfill_query_request(location, description, time):
     #do some work . . . . .
     if response is None:
         return None #todo handle error
-    response_dict = {}
-    response_keys = ['businesses','addresses',
-                           'phone numbers','ratings',
-                           'review counts']
 
-    response_values = []
-    for key in response_keys:
-        response_values.append(list(business.__dict__[key] for business in blist))
-    print response_values
+    response_dict = {} #start from scratch for now but maybe later
+                       #store dict in cookies to reload for returning user
+    for business in response.businesses:
+        if business.name in response_dict:
+            #skip if business is already in dictionary
+            pass
+        else:
+            #add to dictionary
+            response_dict[business.name] = {}
+        response_dict[business.name]['address'] = business.location.display_address
+        response_dict[business.name]['phone number'] = business.phone
+        response_dict[business.name]['rating'] = business.rating
+        response_dict[business.name]['review count'] = business.review_count
+
+    #test our dictionary
+    for name in response_dict.viewkeys():
+        print "name: " + name
+        print "address: "
+        for address_line in response_dict[name]['address']:
+            print address_line
+        print "phone: " + response_dict[name]['phone number']
+
+    #return something cool here
+    return render_template('layout.html')
 
 
 def authenticate_api():
